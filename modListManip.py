dbca393bde70a1d9f585cb0252fc824d0418beac
GiridharGNair <giridhargnair93@gmail.com>
2016-03-06 05:07:46 +0530

    simple module list manipulator

--- modListManip.py
+++ modListManip.py
@@ -1,4 +1,6 @@
 # function arguments are module list from peer, peer's username and file name of tracker's module list file.
+# assumes that module list is received from peer as [ modName1, modName2 ]
+# module list is stored as { "modName1" : [ "peerName1", "peerName2" ], "modName2" : [ "peerName3", "peerName4" ] }
 
 def modListManipulator(peerModList, peerName, trackerModListFileName):
 	trackerModList = pickle.loads(readFile(trackerModListFileName))
