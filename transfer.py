87cc9e40a630cd1ea1462a8506cc471b832a00b9
vasthav <sreevasthavan@gmail.com>
2016-03-06 22:46:08 +0530

    Added transfer.py

--- transfer.py
+++ transfer.py
@@ -1,30 +1,36 @@
 # module for transferring files - sizes upto 4GB
 # call sender and receiver only
 # recvall is an internal function to mirror sendall
+
 import struct
 
+# func to send file
 def send_file(sock, fileName):
 	with open(fileName, "rb") as inFile:
 		data =  inFile.read()
 		sender(sock, data)
 
+# func to recv file
 def recv_file(sock, fileName):
 	with open(fileName, "wb+") as output:
 		writeData = receiver(sock)
 		output.write(writeData)
 
+# data sender
 def sender(sock, data):
 	fileSize = len(data)
 	#print("File size is "+str(fileSize))
 	sock.sendall(struct.pack('!I', fileSize))
 	sock.sendall(data)
 
+# deta reciever
 def receiver(sock):
 	lengthbuf = recvall(sock, 4)
 	length, = struct.unpack('!I', lengthbuf)
 	#print(str(length))
 	return recvall(sock, length)
 
+# sendall mirror func
 def recvall(sock, count):
 	buf = b''
 	while count:
